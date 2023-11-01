842d8fa036dc37924bcdc52079430245beb621a8
HieuShinDepTrai <90301374+HieuShinDepTrai@users.noreply.github.com>
2023-11-01 09:05:22 +0700

    first commit

---
+++ PETrial/Controllers/DirectorController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PETrial.Models;
+
+namespace PETrial.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DirectorController : Controller
+    {
+        private readonly PE_PRN_Fall22B1Context _context;
+        public DirectorController(PE_PRN_Fall22B1Context context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetDirectors/{nationality}/{gender}")]
+        public IActionResult GetDirectors(string nationality, string gender)
+        {
+            var result = _context.Directors.Where(x=>x.Nationality.ToLower() == nationality.ToLower()
+            && x.Male == (gender.ToLower() == "male")).ToList();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            var result1 = result.Select(x => new DirectorDTO()
+            {
+                Id = x.Id,
+                FullName = x.FullName,
+                Description = x.Description,
+                Dob = x.Dob,
+                DobString = x.Dob.ToString("MM/dd/yyyy"),
+                Nationality = x.Nationality,
+                Gender = x.Male == true ? "Male" : "Female"
+            });
+            return Ok(result1);
+        }
+
+        [HttpGet("GetDirector/{id}")]
+        public IActionResult GetDirector(int id)
+        {
+            var res = _context.Directors.Include(x => x.Movies).ThenInclude(x => x.Producer).FirstOrDefault(x => x.Id == id);
+            if (res == null)
+            {
+                return NotFound();
+            }
+            var result = new DirectorDTOwithMovies
+            {
+                Id = res.Id,
+                FullName = res.FullName,
+                Description = res.Description,
+                Dob = res.Dob,
+                DobString = res.Dob.ToString("MM/dd/yyyy"),
+                Nationality = res.Nationality,
+                Gender = res.Male == true ? "Male" : "Female",
+                Movies = res.Movies.Select(x => new MovieDTO()
+                {
+                    Id = x.Id,
+                    Description = x.Description,
+                    DirectorId = x.DirectorId,
+                    DirectorName = x.Director?.FullName,
+                    ProducerId = x.ProducerId,
+                    ProducerName = x.Producer?.Name,
+                    Language = x.Language,
+                    ReleaseDate = x.ReleaseDate,
+                    ReleaseYear = x.ReleaseDate?.Year.ToString(),
+                    Title = x.Title,
+                    Genres = x.Genres,
+                    Stars = x.Stars,
+                }).ToList()
+            };
+            return Ok(result);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(DirectorDTORequest requestDirector)
+        {
+            try
+            {
+                var director = new Director()
+                {
+                    Description = requestDirector.Description,
+                    FullName = requestDirector.FullName,
+                    Dob = Convert.ToDateTime(requestDirector.Dob),
+                    Nationality = requestDirector.Nationality,
+                    Male = requestDirector.Male
+                };
+                _context.Directors.Add(director);
+                var row_inserted = _context.SaveChanges();
+                return Ok(row_inserted);
+            }
+            catch (Exception ex) 
+            { 
+                return Conflict("There is an error while adding"); 
+            }
+        }
+    }
+}
---
+++ PETrial/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using PETrial.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<PE_PRN_Fall22B1Context>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
+builder.Services.AddScoped<PE_PRN_Fall22B1Context>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", builder => builder.AllowAnyMethod().AllowAnyHeader().AllowCredentials().SetIsOriginAllowed((hosts) => true));
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("CORSPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
